3dfd6ea001cb067645ea46e7290b050bbf7a4a85
ArtyomKr <tyomik.krasnitsky@gmail.com>
2022-01-04 12:54:59 +0300

    feat: solved some tasks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -473,8 +473,13 @@ function getIdentityMatrix(n) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const arr = new Array(end - start + 1).fill(0);
+  let i = start - 1;
+  return arr.map(() => {
+    i += 1;
+    return i;
+  });
 }
 
 /**
@@ -488,8 +493,11 @@ function getIntervalArray(/* start, end */) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return arr.filter((item, pos) => {
+    if (pos === 0) return true;
+    return arr.lastIndexOf(item, pos - 1) === -1;
+  });
 }
 
 /**
@@ -540,8 +548,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.map((item) => childrenSelector(item).join()).join().split(',').map((item) => +item || item);
 }
 
 
@@ -557,8 +565,12 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  const index = indexes[0];
+  if (Array.isArray(arr[index])) {
+    return getElementByIndexes(arr[index], indexes.slice(1, indexes.length));
+  }
+  return arr[index];
 }
 
 
@@ -580,8 +592,11 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const half = Math.floor(arr.length / 2);
+  const head = arr.splice(0, half);
+  const tail = arr.splice(-half, half);
+  return tail.concat(arr, head);
 }
 
 
